4b4432498589b970a7924ce57766dc140c79515d
Manisha <manisha23@users.noreply.github.com>
2017-09-08 16:30:36 +0200

    Update ScrapWebsite.java

--- ScrapWebsite.java
+++ ScrapWebsite.java
@@ -21,17 +21,26 @@ public class ScrapWebsite {
 	}
 
 	public static void main(String[] args) throws IOException {
-		String url="http://www.meilleursagents.com";
+		String url="http://www.wikipedia.com";
 		
 		/** Connect to the website and gets total links of URL*/
 		Document doc = Jsoup.connect(url).get();  
 		
 		ScrapWebPage wp = new ScrapWebPage();
-		wp.displayTitle(doc);
-		//wp.displayAllInsideLinks(doc);
-		wp.countNumberOfLinksInside(doc);
+		/**display title*/
+		wp.displayTitle(doc);	
+		
+		/**display all links on the main page*/
+		//wp.displayAllInsideLinks(doc);	
+		
+		/**display number of links found on the page*/
+		wp.countNumberOfLinksInside(doc);	
+		
+		/**display content of the page(for the moment it shows only text of the body of the page)*/
 		wp.scrapPage(doc);
 		
+		/**scrap inside first 3 links found on the main page*/
+		wp.scrapLinksInside(doc,3);
 		
 		/**/
 	}
