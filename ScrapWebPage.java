142db7a9f4378e2c53aad4e812878d47791f0d2c
Manisha <manisha23@users.noreply.github.com>
2017-09-08 16:30:00 +0200

    Update ScrapWebPage.java

--- ScrapWebPage.java
+++ ScrapWebPage.java
@@ -14,20 +14,14 @@ import org.jsoup.select.Elements;
  * @author Manisha
  *
  */
-public class ScrapWebPage extends ScrapWebsite {
+public class ScrapWebPage{
 
 	/**
 	 * 
 	 */
 	public ScrapWebPage() {
 		// TODO Auto-generated constructor stub
-		ScrapWebsite sw  = new ScrapWebsite();
-		return;
-	}
-	
-	public ScrapWebPage(Element link) {
-		// TODO Auto-generated constructor stub
-		Element l= link;
+		
 	}
 
 	/**
@@ -36,7 +30,7 @@ public class ScrapWebPage extends ScrapWebsite {
 	
 	
 	public  void displayAllLinksInside(Document doc) throws IOException {
-		/**Digs into the web page to find all links to other web pages */
+		/**Find all links to other web pages */
 		Elements links = doc.select("a[href]");   //finds links (a tags with href attributes)
 		for (Element link : links) {  
 		    System.out.println("\nlink : " + link.attr("href"));  
@@ -45,6 +39,7 @@ public class ScrapWebPage extends ScrapWebsite {
 	}
 	
 	public void countNumberOfLinksInside(Document doc) {
+		/**Counts number of links found */
 		Elements links = doc.select("a[href]");   //finds links (a tags with href attributes)
 		int i=0;
 		for (Element link : links) {  
@@ -54,33 +49,35 @@ public class ScrapWebPage extends ScrapWebsite {
 	}
 	
 	public  void displayTitle(Document doc) throws IOException {
-		/*Displays the title of the page on console */
+		/**Displays the title of the page on console */
 		String title = doc.title();  
         System.out.println("Title of the webpage is: " + title); 
 	}
 	
 	public  void scrapPage(Document doc) throws IOException {
-		/*Scraps inside the body of the web page */
+		/**Scraps inside the body of the web page */
 		Element body = doc.body();
 		String text = body.text();
 		System.out.println("Text: " + text); 
 	}
 	
 	public void scrapLinksInside(Document doc,int nblinks) throws IOException {
+		/**Scraps into first n links on the webpage and displays information. 
+		 * n is specified as parameter nblinks by the caller class */
 		int n = nblinks;
 		Elements links = doc.select("a[href]");   //finds links (a tags with href attributes)
 		int counter =0;
 		for (Element link : links) {  
 			if (counter < n ) {
-				System.out.println("Link : "+counter);
+				System.out.println("Link : "+link.attr("href"));
 				Document d= Jsoup.connect(link.attr("abs:href")).get();
 				
 				scrapPage(d);
 				counter++;
 			}
-			else {
+			/*else {
 				System.out.println("Nothing more to show");
-			}
+			}*/
 		}
 			
 	}
